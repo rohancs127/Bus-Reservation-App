dcaa63ab5b47f1aec53cfcca59620637b1369b6d
Rohan <rohankumar5803@gmail.com>
2024-09-23 17:34:57 +0530

    create function to format date

---
+++ client/src/scripts/formatDate.js
@@ -0,0 +1,11 @@
+export const formatDate = (isoDate) => {
+  const date = new Date(isoDate);
+  return date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
